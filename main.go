575a72fdeafab4ffb8408e46e0a7372570932f50
jaharbaugh <harbaugh.ja@gmail.com>
2025-11-11 10:52:01 -0600

    Created a repl command repository and added basic help and exit commands

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		words := cleanInput(input)
+		if len(words) == 0{
+			continue
+		}
+
+		if cmd, exists := commandMap[words[0]]; exists {
+			if err := cmd.callback(); err != nil{
+				fmt.Println("Error: ", err)
+			}
+		} else{
+				fmt.Println("Unknown command")
+		}
+	}
 }
--- repl.go
+++ repl.go
@@ -2,10 +2,27 @@ package main
 
 import(
 	"strings"
+	"os"
+	"fmt"
 )
 
 func cleanInput(text string) []string {
 	cleanText := strings.ToLower(text)
 	cleanText = strings.TrimSpace(cleanText)
 	return strings.Fields(cleanText)
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:\n")
+	for _, cmd := range commandMap{
+		fmt.Printf("%s:%s\n", cmd.name, cmd.description)
+	}
+	return nil
 }
